90c95f5ab4794bef6b6544806ab97d126e312e6b
lxy123hh <1677837750@qq.com>
2025-08-09 09:23:56 +0800

    chore: 清空页面只保留底部导航栏and清空commit规范

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,7 @@
 <script setup lang="ts">
-import { onHide, onLaunch, onShow } from '@dcloudio/uni-app'
-import { navigateToInterceptor } from '@/router/interceptor'
-import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only'
-
-onLaunch((options) => {
-  // 处理直接进入页面路由的情况：如h5直接输入路由、微信小程序分享后进入等
-  // https://github.com/unibest-tech/unibest/issues/192
-  console.log('App Launch', options?.path)
-  if (options?.path) {
-    navigateToInterceptor.invoke({ url: `/${options.path}` })
-  }
-  else {
-    navigateToInterceptor.invoke({ url: '/' })
-  }
-})
-onShow(() => {
-  console.log('App Show')
-})
-onHide(() => {
-  console.log('App Hide')
-})
+//
 </script>
 
 <style lang="scss">
-swiper,
-scroll-view {
-  flex: 1;
-  height: 100%;
-  overflow: hidden;
-}
 
-image {
-  width: 100%;
-  height: 100%;
-  vertical-align: middle;
-}
 </style>
